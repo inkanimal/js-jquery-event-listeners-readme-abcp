d94ac99a1a7f914e0dfd9065b672d539492f64d7
geoffrey woodcock <theatomproject@gmail.com>
2018-01-21 18:10:49 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,7 +6,7 @@ function getIt(){
 function frameIt(){
 var img = $("img")
   $("img").on("load", function(){
-    img.addClass("tasty!")
+    img.addClass("tasty")
   });
 }
 $(document).ready(function(){
